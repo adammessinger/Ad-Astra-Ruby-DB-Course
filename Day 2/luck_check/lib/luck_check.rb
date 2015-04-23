ae8929dd582fc15211ae96918fd5532ede3990f9
adammessinger <adam@adammessinger.com>
2015-04-23 12:11:59 -0500

    replaced repetative logic in luck_check with a function

--- Day 2/luck_check/lib/luck_check.rb
+++ Day 2/luck_check/lib/luck_check.rb
@@ -1,19 +1,16 @@
 def luck_check(ticket_number)
   length = ticket_number.length
   part1_end = length/2
-  part2_start = length.odd? ? part1_end + 1 : part1_end
+  part2_start = length.odd?() ? (part1_end + 1) : part1_end
 
   first_half = ticket_number[0...part1_end].split('')
   second_half = ticket_number[part2_start..-1].split('')
 
-  sum1 = 0
-  sum2 = 0
-  first_half.each do |n|
-    sum1 += n.to_i
-  end
-  second_half.each do |n|
-    sum2 += n.to_i
-  end
+  luck_check_half_sum(first_half) == luck_check_half_sum(second_half)
+end
 
-  sum1 == sum2
+def luck_check_half_sum(ticket_half)
+  sum = 0
+  ticket_half.each { |n| sum += n.to_i }
+  sum
 end
