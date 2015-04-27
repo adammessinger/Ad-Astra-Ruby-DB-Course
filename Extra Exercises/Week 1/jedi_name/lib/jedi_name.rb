138527c954c9d59ccb0ebf6acf0f773f6e80cbf9
adammessinger <adam@adammessinger.com>
2015-04-27 00:08:35 -0500

    Removed some unnecessary parentheses. Still getting used to that.

--- Extra Exercises/Week 1/jedi_name/lib/jedi_name.rb
+++ Extra Exercises/Week 1/jedi_name/lib/jedi_name.rb
@@ -1,10 +1,10 @@
 def jedi_name first_name, first_pet_name, birth_city
-  jedi_first = get_name_segment(first_pet_name.reverse)
-  jedi_first += get_name_segment(first_name.reverse)
+  jedi_first = get_name_segment first_pet_name.reverse
+  jedi_first += get_name_segment first_name.reverse
   jedi_first.capitalize!
 
-  jedi_last = get_name_segment(first_name)
-  jedi_last += get_name_segment(first_pet_name)
+  jedi_last = get_name_segment first_name
+  jedi_last += get_name_segment first_pet_name
   jedi_last.capitalize!
 
   planet = birth_city.split(/\W/).last.reverse.capitalize
