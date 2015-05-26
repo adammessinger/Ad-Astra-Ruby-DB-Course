6ee5d3ff2cd9a0b7d703d4568f6ed391d2d8416e
adammessinger <adam@adammessinger.com>
2015-05-26 04:13:57 -0500

    Flesh Out Specs to Test DB Relationships
    order_spec and product_spec expanded to test the HABTM relationship
    between their models' tables.

--- Final Project/spec/order_spec.rb
+++ Final Project/spec/order_spec.rb
@@ -2,5 +2,15 @@ require 'spec_helper'
 
 describe Order do
   it 'has many products' do
+    order  = Order.create(customer_name: 'Janet')
+    burger = Product.all.where(code: 100)
+    fries  = Product.all.where(code: 200)
+    soda   = Product.all.where(code: 300)
+
+    order.products << burger
+    order.products << fries
+    order.products << soda
+
+    expect(order.products.to_a).to(eq [burger[0], fries[0], soda[0]])
   end
 end
--- Final Project/spec/product_spec.rb
+++ Final Project/spec/product_spec.rb
@@ -2,5 +2,15 @@ require 'spec_helper'
 
 describe Product do
   it 'belongs to many orders' do
+    burger = Product.all.where(code: 100)
+    tod_order = Order.create(customer_name: 'Tod')
+    jane_order = Order.create(customer_name: 'Jane')
+
+    burger.orders << tod_order
+    burger.orders << jane_order
+
+    expect(burger.orders).to(eq [tod_order, jane_order])
+    expect(burger.orders.first['customer_name']).to(eq 'Tod')
+    expect(burger.orders.last['customer_name']).to(eq 'Jane')
   end
 end
