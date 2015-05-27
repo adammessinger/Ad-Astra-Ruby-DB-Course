b0695db13f580248a70cebf8ed344e4f9b5d96be
adammessinger <adam@adammessinger.com>
2015-05-27 10:17:50 -0500

    Add Calculation of Tax, Subtotal, Grand Total to Order Model
    All tests in order_spec now pass.

--- Final Project/lib/order.rb
+++ Final Project/lib/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
-  @@tax_rate = 0.07
   has_and_belongs_to_many :products
+  @@tax_rate = 0.07
 
   def self.tax_rate
     @@tax_rate
@@ -11,14 +11,19 @@ class Order < ActiveRecord::Base
   end
 
   def subtotal
+    sub_total = 0
 
+    self.products.each { |product| sub_total += product.price }
+    sub_total
   end
 
   def tax
-
+    self.subtotal * @@tax_rate
   end
 
   def grand_total
+    sub_total = self.subtotal
 
+    sub_total + (sub_total * @@tax_rate)
   end
 end
