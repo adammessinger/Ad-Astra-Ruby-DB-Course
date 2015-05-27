2aeb88319a12fbebc080032438340c2330c201d2
adammessinger <adam@adammessinger.com>
2015-05-27 09:55:24 -0500

    Add Tax Rate and ID Lookup to Order Model

--- Final Project/lib/order.rb
+++ Final Project/lib/order.rb
@@ -1,3 +1,24 @@
 class Order < ActiveRecord::Base
+  @@tax_rate = 0.07
   has_and_belongs_to_many :products
+
+  def self.tax_rate
+    @@tax_rate
+  end
+
+  def self.lookup_id id
+    where id: id
+  end
+
+  def subtotal
+
+  end
+
+  def tax
+
+  end
+
+  def grand_total
+
+  end
 end
