a9063cc7114f7eb38027e1ac818f5f508faff014
adammessinger <adam@adammessinger.com>
2015-05-31 15:06:27 -0500

    Note Future Enhancement Ideas in UI Comments
    * Added TODO comments for future enhancement ideas
    * Changed menu order to spell "MALOX". Kind of like the antacid, but
      with one less A, because the food here isn't quite _that_ bad. ;-P

--- Final Project/register.rb
+++ Final Project/register.rb
@@ -20,10 +20,13 @@ def main_menu
   until choice == 'x'
     puts "\nCOMMANDS"
     puts '--------'
+    puts '"M" to view the Menu' + "\n"
     puts '"A" to Add an order' + "\n"
     puts '"L" to Look up an order by its ID #' + "\n"
-    puts '"O" to list all Orders' + "\n"
-    puts '"M" to view the Menu' + "\n"
+    puts '"O" to list all Orders' + "\n" # TODO: '"U" to list all Unpaid orders'
+    # puts '"P" to list all Paid orders' + "\n" # TODO
+    # puts '"E" to Edit (or delete) an order' + "\n" # TODO
+    # puts '"C" to Cash-out an order (mark paid)' + "\n" # TODO
     puts '"X" to eXit.'
 
     choice = gets.strip.downcase
