ac6b5bf9f1b91ef4ead97cd40f436e9b9ce6b484
adammessinger <adam@adammessinger.com>
2016-01-20 21:47:25 -0600

    Split Jedi First and Last Name Construction into Own Methods
    Cleans up the overly-long jedi_name method.

--- Week 1/Extra Exercises/jedi_name/lib/jedi_name.rb
+++ Week 1/Extra Exercises/jedi_name/lib/jedi_name.rb
@@ -1,21 +1,27 @@
-def jedi_name first_name, first_pet_name, birth_city
+def jedi_name(first_name, first_pet_name, birth_city)
   first_name.strip!
   first_pet_name.strip!
   birth_city.strip!
 
-  jedi_first = get_name_segment first_pet_name.reverse
-  jedi_first += get_name_segment first_name.reverse
-  jedi_first.capitalize!
-
-  jedi_last = get_name_segment first_name
-  jedi_last += get_name_segment first_pet_name
-  jedi_last.capitalize!
-
+  jedi_first = build_jedi_first_name(first_name, first_pet_name)
+  jedi_last = build_jedi_last_name(first_name, first_pet_name)
   planet = birth_city.split(/\W/).last.reverse.capitalize
 
   "#{jedi_first} #{jedi_last} of planet #{planet}"
 end
 
-def get_name_segment word
+def build_jedi_first_name(first_name, first_pet_name)
+  j_first = get_name_segment(first_pet_name.reverse)
+  j_first += get_name_segment(first_name.reverse)
+  j_first.capitalize
+end
+
+def build_jedi_last_name(first_name, first_pet_name)
+  j_last = get_name_segment(first_name)
+  j_last += get_name_segment(first_pet_name)
+  j_last.capitalize
+end
+
+def get_name_segment(word)
   word.length < 3 ? word : /^.*?[aeiouy]+[a-z&&[^aeiouy]]+?/i.match(word).to_s
 end
